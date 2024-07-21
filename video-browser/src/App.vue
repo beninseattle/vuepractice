99e4c8f7ad61e28e0c95d99d9f828c5221098e96
Ben Adair <bena@mac.com>
2024-07-21 06:31:18 +0000

    In progress

---
+++ video-browser/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <SearchBar @termChange="onTermChange"></SearchBar>
+  </div>
+</template>
+
+<script>
+import SearchBar from './components/SearchBar.vue';
+const API_KEY = 'AIzaSyC_XDSswuLfv62z1MI4YGuqLdW69CKsJKs';
+
+export default {
+  name: 'App',
+  components: {
+    SearchBar
+  },
+  methods: {
+    onTermChange(termChange) {
+      console.log('termChange: ', termChange);
+    }
+  }
+};
+</script>
+
+<style></style>
